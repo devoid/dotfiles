dac8224250771c25efb053f768e859b6c172e180
Scott Devoid <scott@d3void.net>
2024-09-05 12:41:10 -0700

    Adding support to trace rebuilds.
    nixos (proust): 26 current  2024-09-05 12:08:04  24.05.4469.6e99f2a27d60  6.6.48                          *

--- nixos/scripts/system-rebuild-trace.sh
+++ nixos/scripts/system-rebuild-trace.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -z $1 ]; then
-    export HOST=$1
-else
-    export HOST=$(hostname)
-fi
-
-sudo nixos-rebuild --show-trace --impure .#$HOST switch
+alias nixos-rebuild="nixos-rebuild --show-trace"
+./system-rebuild.sh

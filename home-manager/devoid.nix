abd4a55e206e7976ef3661948bf1b84749c6526c
Scott Devoid <scott@d3void.net>
2025-10-16 10:32:21 -0700

    home-manager (devoid): 2025-09-15 15:23 : id 58 -> /nix/store/z3qkrh2h3jk55q1bkq8zpcxi7wwgc6vh-home-manager-generation

--- home-manager/devoid.nix
+++ home-manager/devoid.nix
@@ -17,8 +17,6 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # DIFF
-
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs.hyprland;
